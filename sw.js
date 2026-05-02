d0a87c58b6b0a32e5bacef8abfd47b38fb3cb1ec
Shubhankar Basak <shubhankarpc4@gmail.com>
2026-05-02 22:31:34 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = 'notes-v1';
+const urls = [
+  '/NotesApp/',
+  '/NotesApp/index.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urls)));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
